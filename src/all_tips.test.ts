4789a5a9359483f6072e94490efde7b537ac17ae
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-28 20:42:09 +0100

    Simpler test json for tips

--- src/all_tips.test.ts
+++ src/all_tips.test.ts
@@ -1,10 +1,12 @@
 import AllTips from "./all_tips"
+import Tip from "./tip"
 
 describe("AllTips", () => {
   it("returns all the tips from the json file", () => {
     const tips = AllTips("./src/tips.test.json")
 
-    expect(tips[0].text).toBe("Always make an effort to use good variable names.")
-    expect(tips).toHaveLength(3)
+    expect(tips).toHaveLength(2)
+    expect(tips).toContainEqual(new Tip("Tip 1"))
+    expect(tips).toContainEqual(new Tip("Tip 2"))
   })
 })
