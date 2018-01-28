92fa2e936cf9da944345e5d0bb4f948a604d830c
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-01-28 22:16:32 +0100

    chore(mentor): Nicer specs

--- src/tip.test.ts
+++ src/tip.test.ts
@@ -3,13 +3,14 @@ import Tip from "./tip"
 describe("Tip", () => {
   describe("#toMarkdown", () => {
     it("concats the text and the source", () => {
-      expect(new Tip("A tip with a source.", new URL("http://example.com")).toMarkdown()).toEqual(
-        "A tip with a source. Source: [http://example.com/](http://example.com/)"
-      )
+      const tip = new Tip("A tip with a source.", new URL("http://example.com"))
+
+      expect(tip.toMarkdown()).toEqual("A tip with a source. Source: [http://example.com/](http://example.com/)")
     })
 
     it("leaves out the source if not present", () => {
-      expect(new Tip("A tip with a source.").toMarkdown()).toEqual("A tip with a source.")
+      const tip = new Tip("A tip with no source.")
+      expect(tip.toMarkdown()).toEqual("A tip with no source.")
     })
   })
 })
