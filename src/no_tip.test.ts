99254d4178bbf11bbd47fd7ab2b41df1f15e61eb
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-03-03 22:26:43 +0100

    feat(mentor): Show tags with no tip found

--- src/no_tip.test.ts
+++ src/no_tip.test.ts
@@ -3,7 +3,9 @@ import NoTip from "./no_tip"
 describe("NoTip", () => {
   describe("#toMarkdown", () => {
     it("shows a sensible message", () => {
-      expect(new NoTip().toMarkdown()).toEqual("danger-plugin-mentor: No tip found")
+      expect(new NoTip(["nope", "whut"]).toMarkdown()).toEqual(
+        'danger-plugin-mentor: No tip found for tags: "nope", "whut".'
+      )
     })
   })
 })
--- src/no_tip.ts
+++ src/no_tip.ts
@@ -3,7 +3,13 @@ import { Tip } from "./tip"
 export default class NoTip implements Tip {
   readonly tags: string[]
 
+  constructor(tags: string[]) {
+    this.tags = tags
+  }
+
   toMarkdown(): string {
-    return "danger-plugin-mentor: No tip found"
+    const tags = this.tags.map(tag => `"${tag}"`).join(", ")
+
+    return `danger-plugin-mentor: No tip found for tags: ${tags}.`
   }
 }
