ca8c814acfb49bdd2d4258500939e7e9ef715544
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2018-09-22 22:28:46 +0200

    refactor(mentor): One-line two funcs

--- src/random_tip.ts
+++ src/random_tip.ts
@@ -1,18 +1,14 @@
 import AllTips from "./all_tips"
 import NoTip from "./no_tip"
-import { MentorTip, Tip } from "./tip"
+import { Tip } from "./tip"
 
-function rand(max) {
-  return Math.floor(Math.random() * max)
-}
+const rand = max => Math.floor(Math.random() * max)
 
 export default function RandomTip(tags: string[]): Tip {
   let tips = AllTips()
 
   if (tags.length > 0) {
-    tips = tips.filter(tip => {
-      return tip.hasAnyTag(tags)
-    })
+    tips = tips.filter(tip => tip.hasAnyTag(tags))
   }
 
   return tips.length > 0 ? tips[rand(tips.length)] : new NoTip(tags)
